f9ba0ca030f9cc3b29870e6f3ec60b2a3e05424e
euro20179 <sandollar2020@gmail.com>
2023-08-18 20:02:42 -0700

    refactor: modifiers system item: lemonade

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const ingredientHolder = document.getElementById("ingredient-holder") as HTMLDiv
 
 itemSearch.addEventListener("input", e => {
     let regex = /./
-    if(itemSearch.value.startsWith("!")){
+    if (itemSearch.value.startsWith("!")) {
         regex = new RegExp(itemSearch.value.slice(1))
     }
     else {
@@ -15,8 +15,8 @@ itemSearch.addEventListener("input", e => {
     }
     ingredientHolder.querySelectorAll(".ingredient").forEach(elem => {
         let name = elem.getAttribute("data-name") as string
-        if(itemSearch.value.startsWith("!")){
-            if(!name.match(regex)){
+        if (itemSearch.value.startsWith("!")) {
+            if (!name.match(regex)) {
                 elem.classList.remove("hidden")
             }
             else {
@@ -24,7 +24,7 @@ itemSearch.addEventListener("input", e => {
             }
         }
         else {
-            if(!name.match(regex)){
+            if (!name.match(regex)) {
                 elem.classList.add("hidden")
             }
             else {
@@ -54,10 +54,12 @@ const ModifierNames = {
 }
 
 type IngredientHooks = {
-    onCreate?: (el: HTMLDivElement) => any
+    onCreate?: (el: HTMLDivElement) => any,
+    recipeCheck?: (ingList: { [name: string]: Ingredient | ModifierIngredient }) => boolean | "continue"
 }
 
 class Ingredient {
+    pure: boolean = true
     name: string
     image: string | null
     color: string
@@ -79,9 +81,9 @@ class Ingredient {
         this.modifiers = []
     }
 
-    copyWithModifier(modifier: Modifier, newName: string) {
-        let ing = new Ingredient(newName, this.image, this.color, this.textColor, this.hooks)
-        ing.modifiers = this.modifiers
+    copyWithModifier(modifier: Modifier) {
+        let ing = new Ingredient(this.name, this.image, this.color, this.textColor, this.hooks)
+        ing.modifiers = [...this.modifiers] //make a copy of modifiers otherwise when one modifier list is modified the other will be as well
         ing.modifiers.push(modifier)
         return ing
     }
@@ -93,11 +95,44 @@ class Ingredient {
         }
     }
 
-    create() {
-        this.hooks?.onCreate?.(this.getElement())
-        if(!items[this.name as keyof typeof items]){
-            items[this.name as keyof typeof items] = this as any
+    checkRecipe(ingList: Ingredient[]) {
+        if (this.hooks?.recipeCheck) {
+            let res = this.hooks.recipeCheck(Object.fromEntries(ingList.map(v => [v.name, v])))
+            if (res !== 'continue')
+                return res
         }
+        if (this.pure) {
+            for (let ing of ingList) {
+                console.log(ing.modifiers)
+                if (ing.modifiers.length) return false
+            }
+        }
+        if (this.recipe?.length !== ingList.length) {
+            return false
+        }
+        if (ingList.every(v => this.recipe?.includes(v)) && this.recipe?.every(v => ingList.includes(v))) {
+            return true
+        }
+    }
+
+    create() {
+        let dispName = this.getDisplayName()
+        console.log(dispName)
+        if(this.hooks?.onCreate)
+            this.hooks.onCreate(this.getElement())
+        else alert(`You have crafted ${dispName}`)
+        if (!items[dispName as keyof typeof items]) {
+            items[dispName as keyof typeof items] = this as any
+        }
+    }
+
+    getDisplayName() {
+        let displayName = ""
+        for (let mod of this.modifiers) {
+            displayName += ModifierNames[mod] + " "
+        }
+        displayName += ` ${this.name}`
+        return displayName.trim()
     }
 
     getElement() {
@@ -107,7 +142,7 @@ class Ingredient {
         let element = document.createElement("div")
         element.classList.add("ingredient")
         element.setAttribute("data-name", this.name)
-        if(this.recipe?.length){
+        if (this.recipe?.length) {
             let recipieElement = document.createElement("span")
             element.append(recipieElement)
             recipieElement.classList.add("recipie")
@@ -130,8 +165,16 @@ class Ingredient {
             }, 300)
         })
         element.setAttribute("data-name", this.name)
+
+
+        element.innerHTML = this.getDisplayName()
+
         this.#element = element
         return element
+    }
+
+    hasModifier(modifier: Modifier) {
+        return this.modifiers.includes(modifier)
     }
 }
 
@@ -142,17 +185,17 @@ type ModifierIngredientHooks = IngredientHooks & {
 class ModifierIngredient extends Ingredient {
     modifier: Modifier
     hooks?: ModifierIngredientHooks
-    constructor(modifier: Modifier, name: string, image: string | null, color: string, textColor: string | null, hooks?: ModifierIngredientHooks ) {
+    constructor(modifier: Modifier, name: string, image: string | null, color: string, textColor: string | null, hooks?: ModifierIngredientHooks) {
         super(name, image, color, textColor, hooks)
         this.modifier = modifier
         this.hooks = hooks
     }
     modifyIngredients(ing: Ingredient[]) {
-        if(this.hooks?.canModify){
-            if(!this.hooks.canModify.bind(this)(ing))
+        if (this.hooks?.canModify) {
+            if (!this.hooks.canModify.bind(this)(ing))
                 return false
         }
-        let copy = ing[0].copyWithModifier(this.modifier, `${ModifierNames[this.modifier]} ${ing[0].name}`)
+        let copy = ing[0].copyWithModifier(this.modifier)
         copy.setRecipe(this, ...ing)
         return copy
     }
@@ -171,10 +214,10 @@ bowl?.addEventListener("mouseover", e => {
 const rso = new Ingredient("raspberry seed oil", null, "red")
 const water = new Ingredient("water", null, "blue", "white")
 const pepper = new ModifierIngredient(Modifier.Spicy, "pepper", null, 'black', "white", {
-    canModify: function(ing){
-        if(ing.length !== 1) return false
+    canModify: function(ing) {
+        if (ing.length !== 1) return false
         let i = ing[0]
-        if(i.modifiers.includes(this.modifier)) return false
+        if (i.modifiers.includes(this.modifier)) return false
         return true
     }
 })
@@ -183,54 +226,64 @@ const flour = new Ingredient("flour", null, "white", "black")
 const dough = new Ingredient("dough", null, "tan", "black")
 dough.setRecipe(water, flour)
 
-const rw = new Ingredient("raspberry water", null, "red", "blue", {onCreate: el => {
-    el.addEventListener("contextmenu", e => {
-        e.preventDefault()
-        alert("You drink some raspberry water")
-    })
-}})
+const rw = new Ingredient("raspberry water", null, "red", "blue", {
+    onCreate: el => {
+        el.addEventListener("contextmenu", e => {
+            e.preventDefault()
+            alert("You drink some raspberry water")
+        })
+    }
+})
 rw.setRecipe(rso, water)
 
-const garbage = new Ingredient("garbage", null, "green", "white", {onCreate: el => {
-    alert(`You created ${el.getAttribute("data-name")}!!!`)
-}})
-
-const contamination = new Ingredient("contamination", null, "black", "red", {onCreate: el => {
-    alert("Uh oh all your food was spoiled and has been emptied for you")
-    for (let item of playerIngredients) {
-        item.getElement().remove()
+const garbage = new Ingredient("garbage", null, "green", "white", {
+    onCreate: el => {
+        alert(`You created ${el.getAttribute("data-name")}!!!`)
     }
-    playerIngredients = [contamination]
-}})
+})
+
+const contamination = new Ingredient("contamination", null, "black", "red", {
+    onCreate: el => {
+        alert("Uh oh all your food was spoiled and has been emptied for you")
+        for (let item of playerIngredients) {
+            item.getElement().remove()
+        }
+        playerIngredients = [contamination]
+    }
+})
 
 contamination.setRecipe(garbage, water)
 
-const rd = new Ingredient("raspberry dough", null, "white", "red", {onCreate: el => {
-    alert("yummy 😁")
-}})
+const rd = new Ingredient("raspberry dough", null, "white", "red", {
+    onCreate: el => {
+        alert("yummy 😁")
+    }
+})
 
 rd.setRecipe(rw, flour)
 
-const rc = new Ingredient("raspberry cookie", null, "brown", "red", {onCreate: el => {
-    alert("just like grandma used to make ❤️")
-}})
+const rc = new Ingredient("raspberry cookie", null, "brown", "red", {
+    onCreate: el => {
+        alert("just like grandma used to make ❤️")
+    }
+})
 
 rc.setRecipe(rd)
 
 const lemon = new ModifierIngredient(Modifier.Sour, "lemon", null, "yellow", "black", {
-    canModify: function(ing){
-        if(ing.length !== 1) return false
+    canModify: function(ing) {
+        if (ing.length !== 1) return false
         let i = ing[0]
-        if(i.modifiers.includes(this.modifier)) return false
+        if (i.modifiers.includes(this.modifier)) return false
         return true
     }
 })
 
 const sugar = new ModifierIngredient(Modifier.Sweet, "sugar", null, "white", "black", {
     canModify: function(ing) {
-        if(ing.length !== 1) return false
+        if (ing.length !== 1) return false
         let i = ing[0]
-        if(i.modifiers.includes(this.modifier)) return false
+        if (i.modifiers.includes(this.modifier)) return false
         return true
     }
 })
@@ -239,13 +292,25 @@ const soda = new Ingredient("soda", null, "navajowhite", "black")
 soda.setRecipe(sugar, water)
 
 const mud = new Ingredient("mud", null, "#462100", "white", {
-        onCreate: el => {
+    onCreate: el => {
         alert("that's a lot of garbage")
     }
 })
 
 mud.setRecipe(garbage, garbage)
 
+let lemonade = new Ingredient("lemonade", null, "yellow", "black", {
+    recipeCheck: ({ water, lemon, sugar }) => {
+        if (water?.hasModifier(Modifier.Sour) && !lemon && sugar) {
+            return true
+        }
+        if (water?.hasModifier(Modifier.Sweet) && !sugar && lemon) {
+            return true
+        }
+        return 'continue'
+    }
+})
+lemonade.setRecipe(water, lemon, sugar)
 
 let items = {
     "raspberry seed oil": rso,
@@ -283,14 +348,13 @@ craftButton.addEventListener("click", e => {
     for (let item of usedIngredients) {
         if (item instanceof ModifierIngredient) {
             let modified = item.modifyIngredients(usedIngredients.filter(v => v !== item))
-            if(!modified) continue
-            items[modified.name as keyof typeof items] = modified as Ingredient & ModifierIngredient
+            if (!modified) continue
+            items[modified.getDisplayName() as keyof typeof items] = modified as Ingredient & ModifierIngredient
             craftedItems.push(modified)
         }
         if (!item.creates.length) continue
-        for (let [ingList, result] of item.creates) {
-            if(ingList.length !== usedIngredients.length) continue
-            if (ingList.every(v => usedIngredients.includes(v)) && usedIngredients.every(v => ingList.includes(v))) {
+        for (let [_, result] of item.creates) {
+            if (result.checkRecipe(usedIngredients)) {
                 craftedItems.push(result)
             }
         }
@@ -305,8 +369,8 @@ craftButton.addEventListener("click", e => {
         playerIngredients.push(item)
     }
 
-   
+
 })
 emptyButton.addEventListener("click", e => {
-     bowl.replaceChildren("") 
+    bowl.replaceChildren("")
 })
